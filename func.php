07144e6ccf9db947b5988c18d30c33e5b57fc29d
m0pfin <wolg@bk.ru>
2020-01-26 18:15:29 +0300

    Add files via upload

---
+++ func.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: m0pfin
+ * Date: 15.01.2020
+ * Time: 23:19
+ */
+
+$permitted_chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+function generate_string($input, $strength = 16) {
+    $input_length = strlen($input);
+    $random_string = '';
+    for($i = 0; $i < $strength; $i++) {
+        $random_character = $input[mt_rand(0, $input_length - 1)];
+        $random_string .= $random_character;
+    }
+
+    return $random_string;
+}
